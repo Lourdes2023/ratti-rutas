bff2f8ac5ec3db088628e4a921a7d98b42bbfeca
Lourdes2023 <helmanlour@gmail.com>
2023-10-12 20:15:45 -0300

    Se comento la aplicacion de ordenes de axios

--- src/components/UserProfile/Mis-Compras/MisCompras.jsx
+++ src/components/UserProfile/Mis-Compras/MisCompras.jsx
@@ -1,25 +1,24 @@
 import React from "react";
 import { TramiteContainer, StepContent } from "../UserProfile";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { getOrder } from "../../../axios/axiosOrdenes";
-import { clearError, fetchOrderFail } from "../../../redux/ordenes/OrderSlice";
+// import { useEffect } from "react";
+// import { clearError, fetchOrderFail } from "../../../redux/ordenes/OrderSlice";
 import ShippingForm from "../../Form/ShippingForm/ShippingForm";
 const MisCompras = () => {
   const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.user.currentUser);
-  const { ordenes, error } = useSelector((state) => state.ordenes);
-  useEffect(() => {
-    if (!ordenes) {
-      getOrder(dispatch, currentUser);
-    }
+  // const { ordenes, error } = useSelector((state) => state.ordenes);
+  // useEffect(() => {
+  //   if (!ordenes) {
+  //     getOrder(dispatch, currentUser);
+  //   }
 
-    if (!currentUser?.token) {
-      dispatch(fetchOrderFail());
-    } else {
-      error && dispatch(clearError());
-    }
-  }, [ordenes, dispatch, currentUser, error]);
+  //   if (!currentUser?.token) {
+  //     dispatch(fetchOrderFail());
+  //   } else {
+  //     error && dispatch(clearError());
+  //   }
+  // }, [ordenes, dispatch, currentUser, error]);
 
   return (
     <TramiteContainer>
